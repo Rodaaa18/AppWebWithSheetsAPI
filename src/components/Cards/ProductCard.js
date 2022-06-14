e16c2fda1d56269198a2a37d649d28d796ef1324
Rodrigo Alvarez <rodaalvarez18@hotmail.com>
2022-06-14 18:02:56 -0300

    Add Card Component and CSS files

---
+++ src/components/Cards/ProductCard.js
@@ -0,0 +1,48 @@
+//-----------------------------------------------------------------------React
+import React, { useState } from "react";
+//-----------------------------------------------------------------------Bootstrap
+import { Card } from "react-bootstrap";
+//-----------------------------------------------------------------------CSS
+import "./ProductCard.css";
+
+const ProductCard = (props) => {
+  const { content, handleSubmit } = props;
+
+  const [cantidad, setCantidad] = useState(Number(content.CANTIDAD));
+
+  return (
+    <div className="col-md-12">
+      <Card className="card_body">
+        <div className="card-header">
+          <h5 className="card-title">{content.DESCRIPCION}</h5>
+        </div>
+        <div className="card__body">
+          <img src={content.IMAGEN} alt="Imagen del Producto" />
+          Precio Unitario:
+          <p>{content.PRECIO_UNITARIO}</p>
+          <label className="card_body_text">
+            {" "}
+            Cantidad:
+            <input
+              className="cantidad"
+              type="number"
+              value={cantidad}
+              min="0"
+              onChange={(e) => setCantidad(e.target.value)}
+            />
+          </label>
+        </div>
+        <button
+          className="btn__card btn btn-primary glyphicon glyphicon-shopping-cart"
+          type="submit"
+          onClick={() => handleSubmit(content, cantidad)}
+        >
+          {" "}
+          <p id="agregar">Agregar</p>
+        </button>
+      </Card>
+    </div>
+  );
+};
+
+export default ProductCard;
